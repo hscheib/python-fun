3f8abcca0f013b1a2e6d138ac1035a06b16e087a
Hunter Scheib <hunter.scheib@gmail.com>
2025-01-02 20:23:14 +0000

    dockercompose

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
+@app.get("/item/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
