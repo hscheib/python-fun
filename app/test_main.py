d05d17b3942d40bdba269a20db899a3401a5b5d4
Hunter Scheib <hunter.scheib@gmail.com>
2025-01-03 00:23:37 +0000

    updating to earth

--- app/test_main.py
+++ app/test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_hello():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello earth"}
